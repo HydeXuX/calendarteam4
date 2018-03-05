8775932914f704ea18a3287e0904a1ca53d1b9e9
and13020 <and13020@byui.edu>
2018-03-05 10:07:30 -0700

    Added firebase database and accounts

--- app/src/main/java/com/example/mycalendar/data.java
+++ app/src/main/java/com/example/mycalendar/data.java
@@ -1,8 +1,24 @@
 package com.example.mycalendar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 /**
  * Created by ander on 2/28/2018.
  */
 
 public class data {
+
+    // How to get an instance of our database using
+    // get instance() and reference location to write to.
+    // Can save objects this way as well
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = database.getReference("message");
+
+    public void saveData() {
+        myRef.setValue("Hello, World!");
+    }
+
 }
