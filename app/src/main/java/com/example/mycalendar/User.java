df470907149eb9c4630452d3fe34ddf8ce41fb4a
Britthl <Britthl924@gmail.com>
2018-03-07 09:35:49 -0700

    added user class

---
+++ app/src/main/java/com/example/mycalendar/User.java
@@ -0,0 +1,32 @@
+package com.example.mycalendar;
+
+/**
+ * Created by britthunterlefevre on 3/7/18.
+ */
+
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String saveUsername(){
+
+        
+    }
+
+}
